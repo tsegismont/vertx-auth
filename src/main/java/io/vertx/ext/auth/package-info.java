2a51b7ad05d03b624ac2d3cc89e51e7f3435ced6
purplefox <timvolpe@gmail.com>
2015-02-17 13:26:19 +0000

    Various improvements to the API and implementation

--- src/main/java/io/vertx/ext/auth/package-info.java
+++ src/main/java/io/vertx/ext/auth/package-info.java
@@ -2,6 +2,8 @@
  * @author <a href="mailto:julien@julienviet.com">Julien Viet</a>
  */
 @GenModule(name = "ext-auth")
+@Document(fileName = "index.adoc")
 package io.vertx.ext.auth;
 
 import io.vertx.codegen.annotations.GenModule;
+import io.vertx.docgen.Document;
